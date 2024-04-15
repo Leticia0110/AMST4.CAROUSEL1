f2836d0aaaf143bdd2d61a7ad72c0137bef0d3fe
Aron <aron.cardoso1@gmail.com>
2024-04-15 14:56:32 -0300

    Firt Commit

---
+++ AMST4.Carousel.MVC/Pages/Shared/_Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AMST4.Carousel.MVC.Pages.Shared
+{
+    public class _CarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
